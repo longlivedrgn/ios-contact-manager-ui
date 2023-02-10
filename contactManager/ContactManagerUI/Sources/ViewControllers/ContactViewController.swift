57f36967273856d6b75831b2e65480a093ffdfd9
longlivedrgn <kimye9808@naver.com>
2023-02-10 13:16:22 +0900

    feat: 연락처 검색하는 기능 추가
    - filtering 상태에서 삭제할 경우, 기존 tableview에 반영되는 코드 추가

--- contactManager/ContactManagerUI/Sources/ViewControllers/ContactViewController.swift
+++ contactManager/ContactManagerUI/Sources/ViewControllers/ContactViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 class ContactViewController: UIViewController {
 
     @IBOutlet private weak var contactsTableView: UITableView!
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var isFiltering: Bool = false
+    var filterredContacts: [Contact] = []
+    
     @IBAction func addContact(_ sender: UIBarButtonItem) {
         let addContactViewController = AddContactViewController()
         addContactViewController.delegate = self
@@ -20,10 +24,14 @@ class ContactViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        makeRandomContact(with: 10)
+        makeRandomContact(with: 1000)
         
         contactsTableView.delegate = self
         contactsTableView.dataSource = self
+        searchBar.delegate = self
+        // searchBar의 cancel 버튼을 보여줄 지 안보여줄 지 정하기
+        searchBar.showsCancelButton = false
+        
     }
 
     private func makeRandomContact(with count: Int) {
@@ -41,7 +49,7 @@ extension ContactViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contacts.count
+        return isFiltering ? filterredContacts.count : contacts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -58,10 +66,17 @@ extension ContactViewController: UITableViewDataSource {
     private func configure(cell: UITableViewCell, at indexPath: IndexPath) -> UIListContentConfiguration {
         var content = cell.defaultContentConfiguration()
         
-        content.text = "\(contacts[indexPath.row].name)(\(contacts[indexPath.row].age))"
-        content.secondaryText = "\(contacts[indexPath.row].phoneNumber)"
-        content.textProperties.font = .systemFont(ofSize: 16)
-        content.secondaryTextProperties.font = .systemFont(ofSize: 16)
+        if isFiltering {
+            content.text = "\(filterredContacts[indexPath.row].name)(\(filterredContacts[indexPath.row].age))"
+            content.secondaryText = "\(filterredContacts[indexPath.row].phoneNumber)"
+            content.textProperties.font = .systemFont(ofSize: 16)
+            content.secondaryTextProperties.font = .systemFont(ofSize: 16)
+        } else {
+            content.text = "\(contacts[indexPath.row].name)(\(contacts[indexPath.row].age))"
+            content.secondaryText = "\(contacts[indexPath.row].phoneNumber)"
+            content.textProperties.font = .systemFont(ofSize: 16)
+            content.secondaryTextProperties.font = .systemFont(ofSize: 16)
+        }
         
         return content
     }
@@ -74,7 +89,13 @@ extension ContactViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        contacts.remove(at: indexPath.row)
+        if isFiltering {
+            guard let contactIndex = contacts.firstIndex(of: filterredContacts[indexPath.row]) else { return }
+            filterredContacts.remove(at: indexPath.row)
+            contacts.remove(at: contactIndex)
+        } else {
+            contacts.remove(at: indexPath.row)
+        }
         tableView.deleteRows(at: [indexPath], with: .fade)
     }
 }
@@ -82,6 +103,40 @@ extension ContactViewController: UITableViewDelegate {
 extension ContactViewController: AddContactViewDelegate {
     
     func reloadTableView() {
+        contactsTableView.reloadData()
+    }
+}
+
+extension ContactViewController: UISearchBarDelegate {
+    
+    // 서치바에서 검색을 시작할 때 호출된다.
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        isFiltering = true
+        searchBar.showsCancelButton = true
+    }
+    
+    // 서치바의 값이 변경될 때마다 호출이된다.
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard let text = searchBar.text else { return }
+        if text == "" {
+            filterredContacts = contacts
+        } else {
+            filterredContacts = contacts.filter { Contact in
+                Contact.name.localizedCaseInsensitiveContains(text)
+            }
+        }
+        contactsTableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    // 서치바에서 취소 버튼을 눌렀을 때 호출된다.
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        isFiltering = false
         contactsTableView.reloadData()
     }
 }
